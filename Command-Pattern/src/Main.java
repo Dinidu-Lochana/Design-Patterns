cced1be5d27c1bb2f3a02bfc39434d0ed3fe8072
Dinidu-Lochana <24.dinidulochana@gmail.com>
2024-10-31 07:13:59 +0530

    Command-Pattern

--- Command-Pattern/src/Main.java
+++ Command-Pattern/src/Main.java
@@ -1,5 +1,57 @@
+import commands.CruiseControlOff;
+import commands.CruiseControlOn;
+import commands.EverythingOffCommand;
+import commands.EverythingOnCommand;
+import hardware.AutoPilot;
+import hardware.CruiseControl;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Tesla tesla = new Tesla();
+        System.out.println(tesla);
+
+         
+
+
+       /*
+
+       Tesla tesla=new Tesla();
+
+        CruiseControl cruiseControl = new CruiseControl();
+
+        tesla.setCommand(
+                0,
+                new CruiseControlOn(cruiseControl),
+                new CruiseControlOff(cruiseControl)
+        );
+
+        System.out.println(tesla);
+
+        tesla.activateSlot(0);
+        tesla.deactivateSlot(0);
+
+        */
+
+        /*
+        Tesla tesla = new Tesla();
+
+        CruiseControl cruiseControl = new CruiseControl();
+        AutoPilot autoPilot = new AutoPilot();
+
+        tesla.setCommand(
+                1,
+                new EverythingOnCommand(autoPilot,cruiseControl),
+                new EverythingOffCommand(autoPilot,cruiseControl)
+
+        );
+
+        System.out.println(tesla);
+
+        tesla.activateSlot(1);
+        tesla.deactivateSlot(1);
+
+         */
     }
 }
--- Command-Pattern/src/Tesla.java
+++ Command-Pattern/src/Tesla.java
@@ -1,2 +1,48 @@
+import commands.Command;
+import commands.NoOpCommand;
+
 public class Tesla {
+
+    Command[] onCommands;
+    Command[] offCommands;
+
+    int slots=3;
+
+    public Tesla(){
+        onCommands = new Command[slots];
+        offCommands = new Command[slots];
+
+        for (int i=0;i<slots;i++){
+            onCommands[i]=new NoOpCommand();
+            offCommands[i]=new NoOpCommand();
+        }
+    }
+
+    public void setCommand(int slot,Command onCommand,Command offCommand){
+        if (onCommand == null || offCommand == null) {
+            throw new IllegalArgumentException("Commands cannot be null!");
+        } else {
+            onCommands[slot] = onCommand;
+            offCommands[slot] = offCommand;
+        }
+    }
+
+    public void activateSlot(int slot) {
+        onCommands[slot].execute();
+    }
+
+    public void deactivateSlot(int slot) {
+        offCommands[slot].execute();
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\n----Tesla-----\n");
+        for (int i = 0; i < slots; i++) {stringBuilder.append("Slot #" + i + " - " + onCommands[i].getClass().getSimpleName() + "(" + onCommands.getClass().getSimpleName() + ")" +
+                    " - " + offCommands[i].getClass().getSimpleName() + "(" + offCommands.getClass().getSimpleName() + ")" +
+                    "\n");
+        }
+        return stringBuilder.toString();
+    }
 }
